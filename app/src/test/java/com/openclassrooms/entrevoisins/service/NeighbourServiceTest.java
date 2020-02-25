88fc40c833fb3d9974d0b5f7b3fe9253c5622566
DoomGoat <DoomGoat@users.noreply.github.com>
2020-02-25 22:15:41 +0100

    Add (NeighbourServiceTest)
    Add de toute les fonctions de l'ApiService dans les tests unitaires.
    
    Add d'une fake list de voisin favoris pour le test getFavoritesNeighbours.

--- app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
+++ app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test on Neighbour service
@@ -39,5 +40,37 @@ public class NeighbourServiceTest {
         Neighbour neighbourToDelete = service.getNeighbours().get(0);
         service.deleteNeighbour(neighbourToDelete);
         assertFalse(service.getNeighbours().contains(neighbourToDelete));
+    }
+
+    @Test
+    public void createNeighbourWithSuccess() {
+        Neighbour neighbourToCreate = new Neighbour(System.currentTimeMillis(),"","","","","",false);
+        service.createNeighbour(neighbourToCreate);
+        assertTrue(service.getNeighbours().contains(neighbourToCreate));
+    }
+
+    @Test
+    public void makeNeighbourFavoriteWithSuccess() {
+        Neighbour neighbourToFavorite = service.getNeighbours().get(0);
+        service.makeFavorite(neighbourToFavorite);
+        assertTrue(service.getNeighbours().get(0).getFavorite());
+    }
+
+    @Test
+    public void unmakeNeighbourFavoriteWithSuccess() {
+        Neighbour neighbourToUnFavorite = service.getNeighbours().get(0);
+        neighbourToUnFavorite.setFavorite(true);
+        service.unmakeFavorite(neighbourToUnFavorite);
+        assertFalse(service.getNeighbours().get(0).getFavorite());
+    }
+
+    @Test
+    public void getFavoriteNeighboursWithSuccess() {
+        List<Neighbour> favoriteNeighbours = service.getNeighbours();
+        for (int i = 0; i < favoriteNeighbours.size(); i++) {
+            favoriteNeighbours.get(i).setFavorite(true);
+        }
+        List<Neighbour> expectedFavoriteNeighbours = DummyNeighbourGenerator.DUMMY_FAVORITE_NEIGHBOURS;
+        assertThat(favoriteNeighbours, IsIterableContainingInAnyOrder.containsInAnyOrder(expectedFavoriteNeighbours.toArray()));
     }
 }
